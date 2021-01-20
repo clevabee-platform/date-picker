7a36f66d582817ce280b5ca265842be64f22c64b
Simon <simonbbyrne@gmail.com>
2021-01-20 14:35:12 +0000

    temp disable dist for use in project

---
+++ dist/types/components/duet-date-picker/date-picker-day.d.ts
@@ -0,0 +1,14 @@
+import { FunctionalComponent } from "../../stencil-public-runtime";
+import { DuetDateFormatter } from "./date-adapter";
+export declare type DatePickerDayProps = {
+  focusedDay: Date;
+  today: Date;
+  day: Date;
+  isHighlighted: boolean;
+  inRange: boolean;
+  onDaySelect: (event: MouseEvent, day: Date) => void;
+  dateFormatter: DuetDateFormatter;
+  onKeyboardNavigation: (event: KeyboardEvent) => void;
+  focusedDayRef?: (element: HTMLButtonElement) => void;
+};
+export declare const DatePickerDay: FunctionalComponent<DatePickerDayProps>;
